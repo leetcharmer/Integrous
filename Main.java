713471b92aed30582b8685f77aa5e6d120bd899e
John Jelinek IV <john@johnjelinek.com>
2010-03-02 00:17:04 -0600

    Added some test logic in the Main method.

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
-import java.util.Date;
-
+import java.text.SimpleDateFormat;
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
-	@SuppressWarnings("deprecation")
+	
 	public static void main(String[] args) {
 		// Main Method
-		Log item = new Log(new Date( 2010, 2, 1, 20, 49, 0), "Build: int.egro.us",
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+		
+		Log item = new Log( "Build: int.egro.us",
 				"I want to build an application to track one's integrity (also known " +
 				"as the ability to do what they say they will do.)");
 		
-		System.out.println( item.getSubmitDate() + " : " + item.getSummary() );
-		System.out.println( item.getDetails() );
+		System.out.println( dateFormat.format( item.getSubmitDate() ) + " : "
+				+ item.getSummary() + "\n" + item.getDetails() );
 	}
 } // end of class Main
