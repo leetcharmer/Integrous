ead1a6e782f90c08e2dec339299268665ecae7a2
John Jelinek IV <john@johnjelinek.com>
2010-03-01 21:29:19 -0600

    Modified Main Example

--- Main.java
+++ Main.java
@@ -10,9 +10,10 @@ public class Main {
 	public static void main(String[] args) {
 		// Main Method
 		Log item = new Log(new Date( 2010, 2, 1, 20, 49, 0), "Build: int.egro.us",
-				"I want to build an application to track one's integrity (also known) " +
-				"as the ability to do what they say they will do.");
+				"I want to build an application to track one's integrity (also known " +
+				"as the ability to do what they say they will do.)");
 		
-		System.out.println( item.getSummary() + " " + item.getSubmitDate() );
+		System.out.println( item.getSubmitDate() + " : " + item.getSummary() );
+		System.out.println( item.getDetails() );
 	}
 } // end of class Main
