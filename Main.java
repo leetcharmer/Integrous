c8bb364f3fad20f1c009cf36c089dba1ccd1cadc
John Jelinek IV <john@johnjelinek.com>
2010-03-01 20:50:42 -0600

    Added use case example.

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
+import java.util.Date;
+
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Main Method
+		Log item = new Log(new Date( 2010, 2, 1, 20, 49, 0), "Build: int.egro.us",
+				"I want to build an application to track one's integrity (also known) " +
+				"as the ability to do what they say they will do.");
+		
+		System.out.println( item.getSummary() + " " + item.getSubmitDate() );
 	}
-
 } // end of class Main
