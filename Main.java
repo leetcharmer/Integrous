c1b8bb756b19650d56c1394d4c4c0c38497eba41
John Jelinek IV <john@johnjelinek.com>
2010-03-03 21:41:39 -0600

    Created a better implementation test for the Log/User objects.

--- Main.java
+++ Main.java
@@ -1,21 +1,68 @@
-import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
+	private static ArrayList< Log > logItems = new ArrayList< Log >();
+	private static int exit;
 	
-	public static void main(String[] args) {
-		// Main Method
-		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
-		User user = new User( "john@johnjelinek.com" );
+	/** main: Implementation of User / Log objects:
+	 *  1. Ask for Log-In
+	 *  2. Create new User
+	 *  3. Menu Selection
+	 *  	[1. Add Integrity Log]
+	 *  	[-1 to Exit]
+ *  		3a. Validate Selection */
+	public static void main( String[] args ) {
+		// Ask for login address
+		String login = input( "Login E-mail (Hit <ENTER> to Exit): " );
 		
-		Log item = new Log( "Build: int.egro.us",
-				"I want to build an application to track one's integrity (also known " +
-				"as the ability to do what they say they will do.)", user);
-		
-		System.out.println( "+" + item.getUser().getEmail() + "+ " + dateFormat.format( item.getSubmitDate() ) + " : "
-				+ item.getSummary() + "\n" + item.getDetails() );
+		// make sure a valid login is entered
+		if ( !login.equals( "" ) ) {
+			User user = new User( login );
+			
+			try {
+				exit = Integer.parseInt( input( "1. Add Log\n2. Read Logs\n[-1 to Exit]\nChoose: " ) );
+			} catch( NumberFormatException ex ) {
+				exit = 0;
+			}
+			
+			while( exit != -1 ) {
+				try {
+					switch( exit ) {
+						case 1 :
+							Log item = new Log();
+							item.setUser( user );
+							item.setSummary( input( "Enter Summary: " ) );
+							item.setDetails( input( "Enter Details: " ) );
+							
+							logItems.add( item );
+							break;
+						case 2: 
+							for( int index = 0; index < logItems.size(); index++ )
+								System.out.println( logItems.get( index ) + "\n" );
+							break;
+						default : System.out.println( "Invalid Option" );
+					} // end of switch
+					exit = Integer.parseInt(input( "1. Add Log\n2. Read Logs\n[-1 to Exit]\nChoose: " ) );
+				} catch( NumberFormatException e ) { exit = 0; } // end of try / catch
+			} // end of while
+			
+			System.out.println( "GoodBye" );
+		} else { System.out.println( "GoodBye" ); } // end of if / else
+	}
+	
+	/** input: Grab a line of input from the command line */
+	public static String input( String prompt ) {
+		System.out.print( prompt );
+		String input = null;
+		try {
+			input =  new BufferedReader( new InputStreamReader( System.in ) ).readLine().toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			input = null;
+		}
+		return input;
 	}
 } // end of class Main
